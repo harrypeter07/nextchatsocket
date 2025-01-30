e3a27cfc547cdeecbfe349e3901a9027f605428e
harrypeter07 <hassanmansuri570@gmail.com>
2025-01-30 22:55:36 +0530

    ccc

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -8,43 +8,29 @@ export default function SocketHandler(req, res) {
     return;
   }
 
-  const io = new Server(res.socket.server, {
-    path: '/api/socket',
-    cors: { origin: '*' },
-  });
-  
+  const io = new Server(res.socket.server);
   res.socket.server.io = io;
 
   io.use((socket, next) => {
-    try {
-      const token = socket.handshake.auth.token;
-      const user = verifyToken(token);
-      if (user) {
-        socket.user = user;
-        return next();
-      }
+    const token = socket.handshake.auth.token;
+    const user = verifyToken(token);
+    if (user) {
+      socket.user = user;
+      next();
+    } else {
       next(new Error('Authentication error'));
-    } catch (error) {
-      next(new Error('Authentication failed'));
     }
   });
 
   io.on('connection', (socket) => {
-    console.log(`User connected: ${socket.user.username}`);
+    console.log(`User connected: ${socket.user.id}`);
 
     socket.on('message', (message) => {
-      const formattedMessage = {
-        ...message,
-        sender: { _id: socket.user.id, username: socket.user.username },
-      };
-
-      // Send message to sender and broadcast to all other users
-      socket.emit('message', formattedMessage);
-      socket.broadcast.emit('message', formattedMessage);
+      io.emit('message', { ...message, sender: { _id: socket.user.id, username: socket.user.username } });
     });
 
     socket.on('disconnect', () => {
-      console.log(`User disconnected: ${socket.user.username}`);
+      console.log(`User disconnected: ${socket.user.id}`);
     });
   });
 
