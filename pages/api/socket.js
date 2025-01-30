70ec953d6110f784aa51d5dc9fa51997c1f99ba4
harrypeter07 <hassanmansuri570@gmail.com>
2025-01-30 23:30:04 +0530

    socket update

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,3 +1,42 @@
+// import { Server } from 'socket.io';
+// import { verifyToken } from '../../lib/auth';
+
+// export default function SocketHandler(req, res) {
+//   if (res.socket.server.io) {
+//     console.log('Socket is already running');
+//     res.end();
+//     return;
+//   }
+
+//   const io = new Server(res.socket.server);
+//   res.socket.server.io = io;
+
+//   io.use((socket, next) => {
+//     const token = socket.handshake.auth.token;
+//     const user = verifyToken(token);
+//     if (user) {
+//       socket.user = user;
+//       next();
+//     } else {
+//       next(new Error('Authentication error'));
+//     }
+//   });
+
+//   io.on('connection', (socket) => {
+//     console.log(`User connected: ${socket.user.id}`);
+
+//     socket.on('message', (message) => {
+//       io.emit('message', { ...message, sender: { _id: socket.user.id, username: socket.user.username } });
+//     });
+
+//     socket.on('disconnect', () => {
+//       console.log(`User disconnected: ${socket.user.id}`);
+//     });
+//   });
+
+//   console.log('Socket is initialized');
+//   res.end();
+// }
 import { Server } from 'socket.io';
 import { verifyToken } from '../../lib/auth';
 
@@ -8,29 +47,43 @@ export default function SocketHandler(req, res) {
     return;
   }
 
-  const io = new Server(res.socket.server);
+  const io = new Server(res.socket.server, {
+    path: '/api/socket',
+    cors: { origin: '*' },
+  });
+  
   res.socket.server.io = io;
 
   io.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-    const user = verifyToken(token);
-    if (user) {
-      socket.user = user;
-      next();
-    } else {
+    try {
+      const token = socket.handshake.auth.token;
+      const user = verifyToken(token);
+      if (user) {
+        socket.user = user;
+        return next();
+      }
       next(new Error('Authentication error'));
+    } catch (error) {
+      next(new Error('Authentication failed'));
     }
   });
 
   io.on('connection', (socket) => {
-    console.log(`User connected: ${socket.user.id}`);
+    console.log(`User connected: ${socket.user.username}`);
 
     socket.on('message', (message) => {
-      io.emit('message', { ...message, sender: { _id: socket.user.id, username: socket.user.username } });
+      const formattedMessage = {
+        ...message,
+        sender: { _id: socket.user.id, username: socket.user.username },
+      };
+
+      // Send message to sender and broadcast to all other users
+      socket.emit('message', formattedMessage);
+      socket.broadcast.emit('message', formattedMessage);
     });
 
     socket.on('disconnect', () => {
-      console.log(`User disconnected: ${socket.user.id}`);
+      console.log(`User disconnected: ${socket.user.username}`);
     });
   });
 
